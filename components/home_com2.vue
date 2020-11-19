87ac52b8bd0cac9e9089c195176f799ee920eafa
Sakarn <macsakarn@gmail.com>
2020-11-19 15:08:30 +0700

    ขาด รูป

--- components/home_com2.vue
+++ components/home_com2.vue
@@ -56,6 +56,100 @@
     <h2 class="game-ad-banner fab-dl-sec-title">
       Game Features
     </h2>
+    <div class="pc-ch__swiper game-ad-swiper">
+      <div class="pc-ch__container">
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s1.png" class="pc-ch__img" />
+        </div>
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s2.png" class="pc-ch__img" />
+        </div>
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s3.png" class="pc-ch__img" />
+        </div>
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s4.png" class="pc-ch__img" />
+        </div>
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s5.png" class="pc-ch__img" />
+        </div>
+        <div class="pc-ch__slide">
+          <img src="../assets/new_home/image_s6.png" class="pc-ch__img" />
+        </div>
+      </div>
+      <div class="pc-ch__bar">
+        <div class="pc-ch__prev">
+          <img src="../assets/new_home/image_back.png" />
+        </div>
+        <div class="pc-ch__pagination">
+          <div class="pc-ch__pagination-bullet"></div>
+          <div class="pc-ch__pagination-bullet"></div>
+          <div class="pc-ch__pagination-bullet"></div>
+          <div class="pc-ch__pagination-bullet"></div>
+          <div class="pc-ch__pagination-bullet"></div>
+          <div class="pc-ch__pagination-bullet"></div>
+        </div>
+        <div class="pc-ch__next">
+          <img src="../assets/new_home/image_next.png" />
+        </div>
+      </div>
+    </div>
+    <div class="fab-follow-us">
+      <h2 class="follow-us fab-dl-sec-title">
+        Follow us
+      </h2>
+      <ul class="sns-links">
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="fb"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="tw"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="yt"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="ins"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="discord"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="reddit"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/Genshinimpact/"
+            class="forum"
+            target="_blank"
+          ></a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -141,6 +235,67 @@ export default {};
       }
     }
   }
+  .game-ad-swiper {
+    display: block;
+  }
+  .fab-follow-us {
+    width: 100%;
+    height: 1100px;
+    padding-top: 360px;
+    margin-top: -200px;
+    background: url("../assets/new_home/bg_3.png") top center no-repeat;
+    background-image: url("../assets/new_home/bg_3.png");
+    .sns-links {
+      display: flex;
+      justify-content: center;
+      li {
+        opacity: 0.6;
+        transition: all 0.3s;
+        &:hover {
+          opacity: 1;
+        }
+      }
+      li {
+        margin-right: 54px;
+      }
+      a {
+        display: block;
+        height: 123px;
+        background: center/contain no-repeat;
+        background-image: none;
+      }
+      a.fb {
+        width: 94px;
+        background-image: url("../assets/new_home/follow_facebook.png");
+      }
+      a.tw {
+        width: 89px;
+        background-image: url("../assets/new_home/follow_twitter.png");
+      }
+      a.yt {
+        width: 89px;
+        background-image: url("../assets/new_home/follow_youtube.png");
+      }
+      a.ins {
+        width: 99px;
+        height: 128px;
+        background-image: url("../assets/new_home/follow_ig.png");
+      }
+      a.discord {
+        width: 89px;
+        background-image: url("../assets/new_home/follow_discord.png");
+      }
+      a.reddit {
+        width: 89px;
+        background-image: url("../assets/new_home/follow_reddit.png");
+      }
+      a.forum {
+        width: 89px;
+        height: 124px;
+        background-image: url("../assets/new_home/follow_forum.png");
+      }
+    }
+  }
 }
 .fab-dl-sec-title {
   width: 100%;
@@ -154,5 +309,86 @@ export default {};
   -moz-user-select: none;
   -ms-user-select: none;
   user-select: none;
+}
+.pc-ch__container {
+  display: flex;
+  position: relative;
+  margin: 0 auto;
+  width: 1192px;
+  height: 465px;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 11px;
+    width: 100%;
+    height: 100%;
+    background: url("../assets/new_home/image_f.png") center/contain no-repeat;
+    z-index: 10;
+  }
+}
+.pc-ch__slide {
+  width: 845px;
+  height: 100%;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 185px;
+  transition: all 0.3s;
+}
+.pc-ch__img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: 814px;
+  height: 426px;
+}
+.pc-ch__bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 44px;
+}
+.pc-ch__prev,
+.pc-ch__next {
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  outline: none;
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    transition: -webkit-transform 0.3s;
+    transition: transform 0.3s;
+    transition: transform 0.3s, -webkit-transform 0.3s;
+    &:hover {
+      -webkit-transform: scale(1.2);
+      transform: scale(1.2);
+    }
+  }
+}
+.pc-ch__pagination {
+  margin: 0 25px;
+}
+.pc-ch__pagination-bullet {
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  outline: none;
+  opacity: 1;
+  cursor: pointer;
+  background: url("../assets/new_home/image_dot1.png") center/cover no-repeat;
+  &:hover {
+    background: url("../assets/new_home/image_dot2.png") center/cover no-repeat;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,88 @@
 <template>
   <div class="content">
+    <div class="activity-box" v-if="scroll > 300">
+      <a href="">
+        <img src="../assets/new_home/modal.png" alt="" />
+      </a>
+    </div>
     <home_com1 />
     <btn_play @click.native="modalOpen = true" />
     <model_play v-if="modalOpen" @close="modalOpen = false" />
     <home_com2 />
+    <div class="footer">
+      <div class="footer_body">
+        <div class="footer_wrap">
+          <div class="flex items-center">
+            <div class="flex-1">
+              <div class="image_1 inline">
+                <img
+                  src="https://uploadstatic-sea.mihoyo.com/hk4e/upload/officialsites/202004/mihoyo_1586770772_9709.png"
+                  class="inline"
+                />
+              </div>
+              <div class="image_2 inline">
+                <img
+                  src="https://uploadstatic-sea.mihoyo.com/hk4e/upload/officialsites/202004/GIEN_1587984203_6764.png"
+                  class="inline"
+                />
+              </div>
+            </div>
+            <div class="flex-1">
+              <div class="links mb-5">
+                <a
+                  class="link"
+                  href="https://genshin.mihoyo.com/en/company/privacy"
+                  target="_blank"
+                >
+                  Privacy Policy
+                </a>
+                <a
+                  class="link"
+                  href="https://genshin.mihoyo.com/en/company/terms"
+                  target="_blank"
+                >
+                  Terms of Service
+                </a>
+                <a
+                  class="link"
+                  href="https://genshin.mihoyo.com/en/company/about"
+                  target="_blank"
+                >
+                  About Us
+                </a>
+                <a
+                  class="link"
+                  href="mailto:genshin_cs@mihoyo.com"
+                  target="_blank"
+                >
+                  Contact Us
+                </a>
+              </div>
+
+              <div class="copyright">
+                Copyright© 2012-2020 miHoYo ALL RIGHTS RESERVED
+              </div>
+              <div class="extra-info">
+                "PlayStation Family Mark" and "PS4 logo" are registered
+                trademarks or trademarks of Sony Interactive Entertainment Inc.
+              </div>
+              <div class="icon_footer flex pt-5">
+                <img
+                  src="../assets/footer/age.png"
+                  style="margin-right: 10px; margin-bottom: 10px"
+                />
+                <img src="../assets/footer/teen.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import debounce from "lodash/debounce";
 export default {
   components: {
     home_com1: () => import("../components/home_com1"),
@@ -17,8 +92,21 @@ export default {
   },
   data() {
     return {
-      modalOpen: false
+      modalOpen: false,
+      scroll: 0
     };
+  },
+  methods: {
+    handleScroll() {
+      // Your scroll handling here
+      this.scroll = window.scrollY;
+    }
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
@@ -78,6 +166,59 @@ export default {
     background-position-x: 0%;
     background-position-y: 0%;
     background-position: center bottom;
+  }
+}
+.activity-box {
+  position: fixed;
+  bottom: 100px;
+  right: 20px;
+  width: 269px;
+  height: 341px;
+  z-index: 1000;
+  a,
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.footer {
+  position: relative;
+  z-index: 998;
+  width: 100%;
+  min-width: 1280px;
+  background-color: #111;
+  margin: 0 auto;
+
+  .footer_body {
+    background-color: #111;
+    padding: 30px 0 50px;
+
+    .footer_wrap {
+      width: 1000px;
+      margin: 0 auto;
+      padding: 0 12px;
+      color: #999;
+      box-sizing: border-box;
+
+      .links a.link {
+        padding: 0 10px;
+        border-left: 1px solid #fff;
+        line-height: 1;
+      }
+
+      .copyright {
+        color: #fff;
+      }
+
+      .extra-info {
+        color: #7d7d7d;
+        font-size: 14px;
+        line-height: 20px;
+        padding: 10px 0;
+      }
+    }
   }
 }
 </style>
